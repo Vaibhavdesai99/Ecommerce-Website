c1071aaa1e865237cf4d7f9a87b20a0b9e3753d7
Vaibhavdesai99 <vaibhavdesai510@gmail.com>
2023-05-20 23:49:46 +0530

    Implement the About page in the most optimised way

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Summary from "./Components/Summary/Summary";
 import Context from "./Components/Store/Context";
 import Productlist from "./Components/ProductList/Productlist";
 import Footer from "./Components/Footer/Footer";
+import About from "./Components/About/About";
+
+const router = createBrowserRouter([{ path: "/About", element: <About /> }]);
 
 const App = () => {
   return (
-    <Context>
-      <Header />
-      <Summary />
-      <Productlist />
-      <Footer />
-    </Context>
+    <>
+      <Context>
+        <RouterProvider router={router} />
+        <Header />
+        <Summary />
+        <Productlist />
+        <Footer />
+      </Context>
+    </>
   );
 };
 
---
+++ src/Components/About/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <p>I AM THE ABOUT PAGE HERE </p>
+    </div>
+  );
+};
+
+export default About;
